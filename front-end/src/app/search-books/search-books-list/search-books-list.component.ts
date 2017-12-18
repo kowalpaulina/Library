06b67528bd2870f5e3ac94843a6b677418e46496
kowalpaulina <kowalpaulina@gmail.com>
2017-12-18 23:10:12 +0100

    style changes

--- front-end/src/app/search-books/search-books-list/search-books-list.component.ts
+++ front-end/src/app/search-books/search-books-list/search-books-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { BooksService } from '../../books/books.service'
 import { BooksSearchService } from '../books-search.service'
+import { AuthService } from "../../login/auth.service";
 
 @Component({
   selector: 'app-search-books-list',
@@ -8,23 +9,30 @@ import { BooksSearchService } from '../books-search.service'
   styleUrls: ["./search-books-list.component.scss"],
 })
 export class SearchBooksListComponent implements OnInit {
+    isLogged: boolean;
 
 
-  constructor(private booksService:BooksService, private booksSearchService:BooksSearchService) { }
+  constructor(private booksService:BooksService, private booksSearchService:BooksSearchService, private authService: AuthService) {
+    this.authService.isLoggedIn.subscribe(value => {
+      this.isLogged = value;
+    });
+   }
 
   chosenBook={};
 
   @Input()
   books;
+  message: string = "Click chosen book to add it to your book collection";
  
+
 
   chosen(chosenBook){
     console.log("kliknięta ksiązka z szukanych",chosenBook);
     this.booksService.addBookToLibrary(chosenBook);
   }
 
-  
   ngOnInit() {
+
   }
 
 }
